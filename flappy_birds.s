749094191ad176e27c94d4e26aa14e05c76239b3
Ibrahim <ibrahim@Ibbyyy.localdomain>
2020-03-26 17:15:31 -0400

    w/e

--- flappy_birds.s
+++ flappy_birds.s
@@ -7,7 +7,20 @@
 	li $t2, 0xffd000	# $t2 stores the bird colour
 	add $t3, $zero, $zero 	#counter variable
 	li $t4, 0x008900	#pipe colors
-	add $t6, $zero, $zero 	#used for animation
+	add $t6, $zero, $t0 	#this is the actual address use
+	add $t7, $zero, $t6
+	add $t7, $t7, 1920
+main: 
+	jal loop
+	li $a0, 3000
+	li $v0, 32
+	syscall
+	
+	lw $t0, displayAddress
+	add $t6, $zero, $t0
+	add $t6, $t6, 90
+	
+	j topPipe
 	
 	
 loop:	beq $t3, 4096, middle 	#this makes the screen blue
@@ -16,78 +29,55 @@ loop:	beq $t3, 4096, middle 	#this makes the screen blue
 	add $t3, $t3, 4
 	j loop
 
-middle:	lw $t0, displayAddress
-	add $t0, $t0, 1920
-
+middle:	#lw $t0, displayAddress
+	#add $t0, $t0, 1920
+	#add $t6, $t6, 1920
 	
-bird: 	sw $t2, 0($t0)		#this makes the bird
-	sw $t2, -128($t0)
-	sw $t2, 128($t0)
-	sw $t2, 4($t0)
-	sw $t2, 8($t0)
-	sw $t2, -120($t0)
-	sw $t2, 136($t0)
-	sw $t2, 12($t0)
+bird: 	sw $t2, 0($t7)		#this makes the bird
+	sw $t2, -128($t7)
+	sw $t2, 128($t7)
+	sw $t2, 4($t7)
+	sw $t2, 8($t7)
+	sw $t2, -120($t7)
+	sw $t2, 136($t7)
+	sw $t2, 12($t7)
 
 	lw $t0, displayAddress
-	add $t0, $t0, 92
-topPipe:
-	#sw $t4, 100($t0)
-	#sw $t4, 96($t0)
-	#sw $t4, 92($t0)
-	#sw $t4, 228($t0)
-	#sw $t4, 224($t0)
-	#sw $t4, 220($t0)
+	add $t6, $zero, $t0
+	add $t6, $t6, 92
 	
+topPipe:
 	li $t3, 0
 	
 Outer:	beq $t3, 10, lowerPipe
 		li $t5, 0
 Inner:		beq $t5, 3, Incre
-			sw $t4, 0($t0)
+			sw $t4, 0($t6)
 			add $t5, $t5, 1
-			add $t0, $t0, 4
+			add $t6, $t6, 4
 			j Inner
-Incre:		add $t0, $t0, 116
+Incre:		add $t6, $t6, 116
 		add $t3, $t3, 1
 		j Outer
 	
 
 lowerPipe:
-	add $t0, $t0, 768
+	add $t6, $t6, 768
 	li $t3, 0
 	
 OuterLower:	beq $t3, 16, Exit
 		li $t5, 0
 InnerLower:		beq $t5, 3, IncreLower
-			sw $t4, 0($t0)
+			sw $t4, 0($t6)
 			add $t5, $t5, 1
-			add $t0, $t0, 4
+			add $t6, $t6, 4
 			j InnerLower
-IncreLower:		add $t0, $t0, 116
+IncreLower:		add $t6, $t6, 116
 		add $t3, $t3, 1
 		j OuterLower
-	
-animation: 
-		li $t3, 0
-		lw $t0, displayAddress
-		beq $t3, $t6, nothing
-			add $t3, $t3, 1
-			add $t0, $t0, -4
-		jr $ra
 
-nothing: 	jr $ra
 
 Exit:
 	li $v0, 10 # terminate the program gracefully
 	syscall
 
-main: 
-	j loop
-	add $t6, $t6, 1
-	addi $a0, $zero, 1000
-	addi $v0, $zero, 32
-	syscall
-	j loop
-	
-	
